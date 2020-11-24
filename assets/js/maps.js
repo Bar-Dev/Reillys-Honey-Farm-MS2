e161bdb2067ea7fc65e4a5446f4c86430449689a
Bar-Dev <bdprojectservices@gmail.com>
2020-11-24 19:57:18 +0000

    Add JS File for Map

---
+++ assets/js/maps.js
@@ -0,0 +1,24 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 7,
+        center: {
+            lat: 52.1028275,
+            lng: -7.6569345
+        }
+    });
+
+    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    var locations = [
+        { lat: 52.1028275, lng: -7.6569345 } 
+    ];
+
+    var markers = locations.map(function(location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+
+    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+}
